0783b210656b5d47d745e00787511fb2f1cabfea
Rohan <rshetty449@gmail.com>
2021-12-22 19:23:32 -0500

    removed python stuff and simplified getting state

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const {Teams, TeamValidator} = require('pokemon-showdown');
+const {State} = require('pokemon-showdown');
 const Sim = require("pokemon-showdown");
 const fs = require("fs");
 const spawn = require("child_process").spawn;
+const { randomInt } = require('crypto');
 
 // /*
 // Get a random team using the Pokemon Showdown API.
@@ -16,10 +17,17 @@ stream = new Sim.BattleStream();
 
 (async () => {
     for await (var output of stream) {
-        const pp = spawn("python", ["C:/Users/rshet/Documents/IChooseYou.ML/Python/stream_reader.py", "output", output]);
+        // const pp = spawn("python", ["C:/Users/rshet/Documents/IChooseYou.ML/Python/stream_reader.py", "output", output]);
+        fs.writeFileSync("./bin/request.json", JSON.stringify(stream.battle.toJSON(), null, "\t"));
     }
 })();
 
 stream.write(`>start {"formatid":"gen8randombattle"}`);
 stream.write(`>player p1 {"name":"Cock"}`);
 stream.write(`>player p2 {"name":"Balls"}`);
+for (let i=0; i < 30; i++) {
+    stream.write(`>p1 move ` + randomInt(1, 5));
+    stream.write(`>p2 move ` + randomInt(1, 5));
+}
+
+
