cbbfd9c6f57de3a0e38ff23fa2ea26ae7df07b9b
Trevor Du <trevorguanhaodu@gmail.com>
2021-12-28 18:40:30 -0500

    update moves, items, abilities

---
+++ data/aggregator.js
@@ -0,0 +1,29 @@
+/**
+ * Aggregates move, abilities, and item data from randomdata.json
+ * into their own respective files. For encoding purposes.
+ */
+
+const data = require("./randomdata.json");
+const fs = require("fs");
+
+moves = []
+items = []
+abilities = []
+
+for (let species of Object.values(data)) {
+    for (move of species.randomBattleMoves) {
+        if (!moves.includes(move)) moves.push(move);
+    }
+
+    for (item of species.items || []) {
+        if (!items.includes(item)) items.push(item);
+    }
+
+    for (ability of species.abilities || []) {
+        if (!abilities.includes(ability)) abilities.push(ability);
+    }
+}
+
+fs.writeFileSync("moves.json", JSON.stringify(moves), "utf-8");
+fs.writeFileSync("items.json", JSON.stringify(items), "utf-8");
+fs.writeFileSync("abilities.json", JSON.stringify(abilities), "utf-8");
