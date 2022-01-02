731e9fa68387c116707662a8779bc6b7ab4eb18d
Rohan <rshetty449@gmail.com>
2022-01-02 14:33:51 -0500

    add active state for opponent in request

--- index.js
+++ index.js
@@ -1,95 +1,106 @@
-const {State} = require('pokemon-showdown');
+const { State } = require('pokemon-showdown');
 const Sim = require("pokemon-showdown");
 const fs = require("fs");
 const spawn = require("child_process").spawn;
-const { randomInt } = require('crypto');
 const https = require('https');
+var http_server = https.createServer( app ).listen( 3000 );
+var io = require( "socket.io" )( http_server );
 const smogon_api = 'https://smogon-usage-stats.herokuapp.com/2019/12/gen8randombattle/1630/';
 
 const num_games = 32;
 
 // Game instance used to run concurrent games for training
 class Game {
-    constructor(id1, id2) {
+    constructor(id) {
         this.stream = new Sim.BattleStream();
-        // this.process = spawn('python', ['']);
 
         // UID of both models
-        this.p1 = id1;
-        this.p2 = id2;
+        this.id = id;
+
+        // Get live state of game
+        (async () => {
+            for await (var output of this.stream) {
+                // console.log(output)
+
+                let msg = output.split("|");
+
+                if (msg.includes("turn")) {
+                    let data = this.stream.battle.toJSON();
+
+                    // Adding data for each side
+                    data.sides.forEach(side => {
+                        side.volatiles = [];
+                        side.opponent = {};
+                        let other = side == data.sides[0] ? data.sides[1] : data.sides[0];
+
+                        side.pokemon.forEach(mon => {
+                            // Get HP% for each pokemon
+                            mon.percenthp = 1.0 * mon.hp / mon.maxhp // ensure float division
+
+                            // Create list of volatiles for each side
+                            for (let i = 0; i < Object.keys(mon.volatiles).length; i++) {
+                                side.volatiles.push(mon.volatiles[Object.keys(mon.volatiles)[i]]);
+                            }
+                        });
+
+                        // TODO only include known pokemon
+                        // Set opposing pokemon data
+                        side.opponent.volatiles = [];
+                        side.opponent.sideConditions = other.sideConditions;
+                        side.opponent.dynamaxUsed = other.dynamaxUsed;
+                        side.opponent.pokemon = {};
+                        other.pokemon.forEach(mon => {
+                            for (let i = 0; i < Object.keys(mon.volatiles).length; i++) {
+                                side.opponent.volatiles.push(mon.volatiles[Object.keys(mon.volatiles)[i]]);
+                            }
+                            side.opponent.pokemon[mon.speciesState.id] = {};
+                            side.opponent.pokemon[mon.speciesState.id].name = mon.speciesState.id;
+                            side.opponent.pokemon[mon.speciesState.id].isActive = mon.isActive;
+                            side.opponent.pokemon[mon.speciesState.id].types = mon.types;
+                            side.opponent.pokemon[mon.speciesState.id].percenthp = 1.0 * mon.hp / mon.maxhp;
+                            side.opponent.pokemon[mon.speciesState.id].baseStats = dex[mon.speciesState.id].baseStats;
+                            side.opponent.pokemon[mon.speciesState.id].abilities = mon.boosts;
+                            side.opponent.pokemon[mon.speciesState.id].moves = genericInfo[mon.speciesState.id].randomBattleMoves;
+                            side.opponent.pokemon[mon.speciesState.id].items = genericInfo[mon.speciesState.id].items;
+                            side.opponent.pokemon[mon.speciesState.id].abilities = genericInfo[mon.speciesState.id].abilities;
+                        });
+                    });
+
+                    // Write data to request file for model use
+                    fs.writeFileSync("./bin/request_" + this.id + ".json", JSON.stringify(data, null, "\t"));
+
+                }
+            }
+        })();
+
+        // START GAME
+        this.stream.write(`>start {"formatid":"gen8randombattle"}`);
+        this.stream.write(`>player p1 {"name":"Cock"}`);
+        this.stream.write(`>player p2 {"name":"Balls"}`);
     }
 
     move(p) {
         // call model here
-    
+
         // s.write(`>p` + p + ` move 1`); // model choice
     }
 }
 
+//instantiate python process
+// let py = spawn("python", ["training.py"]);
+
+//socket.io stuff
+// io.on( "connection", function( socket ) {
+//     socket.on( 'python-message', function( fromPython ) {
+//         socket.broadcast.emit( 'message', fromPython );
+//     });
+// })
+
 const dex = JSON.parse(fs.readFileSync('./data/pokedex.json'));
 const genericInfo = JSON.parse(fs.readFileSync('./data/randomdata.json'));
 
-let game = new Game(1, 2);
+let games = []; // list of running games
 
-// Get live state of game
-(async () => {
-    for await (var output of game.stream) {
-        // console.log(output)
-
-        let msg = output.split("|");
-
-        if (msg.includes("turn")) {
-            let data = game.stream.battle.toJSON();
-
-            // Adding data for each side
-            data.sides.forEach(side => {         
-                side.volatiles = [];
-                side.opponent = {};
-                let other = side == data.sides[0] ? data.sides[1] : data.sides[0];
-
-                side.pokemon.forEach(mon => {
-                    // Get HP% for each pokemon
-                    mon.percenthp = 1.0 * mon.hp / mon.maxhp // ensure float division
-
-                    // Create list of volatiles for each side
-                    for (let i=0; i < Object.keys(mon.volatiles).length; i++) {
-                        side.volatiles.push(mon.volatiles[Object.keys(mon.volatiles)[i]]);
-                    }
-                });
-
-                // TODO only include known pokemon
-                // Set opposing pokemon data
-                side.opponent.volatiles = [];
-                side.opponent.sideConditions = other.sideConditions;
-                side.opponent.dynamaxUsed = other.dynamaxUsed;
-                side.opponent.pokemon = {};
-                other.pokemon.forEach(mon => {
-                    for (let i=0; i < Object.keys(mon.volatiles).length; i++) {
-                        side.opponent.volatiles.push(mon.volatiles[Object.keys(mon.volatiles)[i]]);
-                    }
-                    side.opponent.pokemon[mon.speciesState.id] = {};
-                    side.opponent.pokemon[mon.speciesState.id].name = mon.speciesState.id;
-                    side.opponent.pokemon[mon.speciesState.id].types = mon.types;
-                    side.opponent.pokemon[mon.speciesState.id].percenthp = 1.0 * mon.hp / mon.maxhp;
-                    side.opponent.pokemon[mon.speciesState.id].baseStats = dex[mon.speciesState.id].baseStats;
-                    side.opponent.pokemon[mon.speciesState.id].abilities = mon.boosts;
-                    side.opponent.pokemon[mon.speciesState.id].moves = genericInfo[mon.speciesState.id].randomBattleMoves;
-                    side.opponent.pokemon[mon.speciesState.id].items = genericInfo[mon.speciesState.id].items;
-                    side.opponent.pokemon[mon.speciesState.id].abilities = genericInfo[mon.speciesState.id].abilities;
-                });
-            });
-
-            // Write data to request file for model use
-            fs.writeFileSync("./data/samplerequest.json", JSON.stringify(data, null, "\t"));
-
-            // CALL MOVES HERE
-        }
-    }
-})();
-
-// TESTING
-game.stream.write(`>start {"formatid":"gen8randombattle"}`);
-game.stream.write(`>player p1 {"name":"Cock"}`);
-game.stream.write(`>player p2 {"name":"Balls"}`);
-game.stream.write(`>p1 default`);
-game.stream.write(`>p2 move 1 max`);
+for (let i = 0; i < num_games; i++) { // create games
+    games.push(new Game(i));
+}
